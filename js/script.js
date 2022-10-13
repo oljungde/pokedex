23b9b6e46ebbd6a6d5ada2e95fac25b8dc3ab776
oljungde <oljung@posteo.de>
2022-10-13 15:01:45 +0200

    get all Pokemon from API

--- js/script.js
+++ js/script.js
@@ -1,8 +1,39 @@
+let pokemonList = [];
 let allPokemon = [];
 
 
-async function getAllPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/';
+async function getPokemonList() {
+    let url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
     let response = await fetch(url);
-    allPokemon = await response.json();
+    pokemonList = await response.json();
+    getAllPokemon()
+}
+
+
+async function getAllPokemon() {
+    for (let i = 0; i < pokemonList['results'].length; i++) {
+        const onePokemon = pokemonList['results'][i];
+        let onePokemonUrl = onePokemon['url'];
+        let response = await fetch(onePokemonUrl);
+        let responseAsJson = await response.json();
+        allPokemon.push(responseAsJson);
+    }
+    renderPokemon();
+}
+
+
+function renderPokemon() {
+    let pokemonContainer = document.getElementById('all_pokemon');
+    for (let i = 0; i < allPokemon.length; i++) {
+        const pokemon = allPokemon[i];
+        let pokemonName = pokemon['name'];
+        let pokemonId = pokemon['id'];
+        let pokemonImage = pokemon['sprites']['other']['official-artwork']['front_default'];
+        pokemonContainer.innerHTML += /*html*/`
+            <div>
+                <h2>${pokemonName} #${pokemonId}</h2>
+                <img src="${pokemonImage}" alt="">
+            </div>
+        `;
+    }
 }
