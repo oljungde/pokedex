772d81483a7b9d916eba6fe8f664d0054d69d470
oljungde <oljung@posteo.de>
2022-10-14 14:07:37 +0200

    new pokemon render function

--- js/script.js
+++ js/script.js
@@ -1,50 +1,43 @@
-let pokemonList = [];
-let allPokemon = [];
-let loadedPokemon = 0;
-let pokemonToLoad = 20;
+let nextPokemonList = 'https://pokeapi.co/api/v2/pokemon/';
+let loadedPokemons = [];
 
-
-async function getPokemonList() {
-    let url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
-    let response = await fetch(url);
-    pokemonList = await response.json();
-    getAllPokemon()
-}
-
-
-async function getAllPokemon() {
-    for (let i = 0; i < pokemonList['results'].length; i++) {
-        const onePokemon = pokemonList['results'][i];
-        let onePokemonUrl = onePokemon['url'];
-        let response = await fetch(onePokemonUrl);
-        let responseAsJson = await response.json();
-        allPokemon.push(responseAsJson);
+async function loadPokemon() {
+    let response = await fetch(nextPokemonList);
+    let responseAsJSON = await response.json();
+    nextPokemonList = responseAsJSON.next;
+    console.log(nextPokemonList);
+    for (let i = 0; i < responseAsJSON.results.length; i++) {
+        let pokemonResponse = await fetch(responseAsJSON.results[i].url);
+        let pokemonAJSON = await pokemonResponse.json();
+        loadedPokemons.push(pokemonAJSON);
     }
-    renderPokemon();
+    renderAllPokemon();
 }
 
 
-function renderPokemon() {
+function renderAllPokemon() {
     let pokemonContainer = document.getElementById('all_pokemon');
-    for (let i = loadedPokemon; i < (loadedPokemon + pokemonToLoad); i++) {
-        const pokemon = allPokemon[i];
+    for (let i = 0; i < loadedPokemons.length; i++) {
+        const pokemon = loadedPokemons[i];
         let pokemonName = pokemon['name'];
-        let pokemonId = pokemon['id'];
         let pokemonImage = pokemon['sprites']['other']['official-artwork']['front_default'];
+        let pokemonType0 = pokemon['types'][0]['type']['name'];
         pokemonContainer.innerHTML += /*html*/`
-            <div>
-                <h2>${pokemonName} #${pokemonId}</h2>
-                <img src="${pokemonImage}" alt="">
-            </div>
+             <div class="one-pokemon ${pokemonType0}">
+                 <h2>${pokemonName}</h2>
+                 <div class="image">
+                    <img src="${pokemonImage}" alt="">
+                 </div>
+                 
+             </div>
         `;
+
     }
-    loadedPokemon = loadedPokemon + pokemonToLoad;
 }
 
 
 window.addEventListener('scroll', function () {
-    console.log(window.scrollY);
     if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
-        renderPokemon();
+        renderAllPokemon();
     }
 })
