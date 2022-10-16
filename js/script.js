27d96d983f13fe38e6afff08b013b66bbd7f69ff
oljungde <oljung@posteo.de>
2022-10-16 16:36:10 +0200

    search

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let themeSwitchBtns;
 let nextPokemonList = 'https://pokeapi.co/api/v2/pokemon/';
 let loadedPokemons = [];
 let currentPokemon = 0;
+let searchedPokemon = [];
 
 
 async function init() {
@@ -29,8 +30,8 @@ async function loadPokemon() {
     nextPokemonList = responseAsJSON.next;
     for (let i = 0; i < responseAsJSON.results.length; i++) {
         let pokemonResponse = await fetch(responseAsJSON.results[i].url);
-        let pokemonAJSON = await pokemonResponse.json();
-        loadedPokemons.push(pokemonAJSON);
+        let pokemonAsJson = await pokemonResponse.json();
+        loadedPokemons.push(pokemonAsJson);
     }
     renderAllPokemon();
 }
@@ -64,3 +65,49 @@ window.addEventListener('scroll', function () {
         loadPokemon();
     }
 })
+
+
+async function getSearchTerm() {
+    let search = document.getElementById('search').value;
+    if (search.length >= 3) {
+        console.log(search);
+        search = search.toLowerCase();
+        loadSearchedPokemon(search);
+    }
+}
+
+
+async function loadSearchedPokemon(searchTerm) {
+    let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
+    console.log(response);
+    let responseAsJson = await response.json();
+    console.log(responseAsJson);
+    for (let i = 0; i < responseAsJson['results'].length; i++) {
+        const pokemon = responseAsJson['results'][i];
+        let pokemonName = pokemon['name'];
+        if (pokemonName.toLocaleLowerCase().includes(searchTerm)) {
+            let pokemonResponse = await fetch(pokemon['url']);
+            let pokemonAsJson = await pokemonResponse.json();
+            searchedPokemon.push(pokemonAsJson);
+            console.log(searchedPokemon);
+        }
+
+    }
+
+}
+
+
+// function filterPosts(searchTerm) {
+//     let pokemonContainer = document.getElementById('all_pokemon');
+//     pokemonContainer.innerHTML = '';
+//     for (let i = 0; i < posts.length; i++) {
+//         let postAuthor = posts[i].author;
+//         let postLocation = posts[i].postLocation;
+//         let postText = `${posts[i].postTextTeaser} ${posts[i].postTextMore}`;
+//         if (postAuthor.toLocaleLowerCase().includes(searchTerm) || postText.toLocaleLowerCase().includes(searchTerm) || postLocation.toLocaleLowerCase().includes(searchTerm)) {
+//             postsContainer.innerHTML += templatePost(i);
+//             renderPost(i);
+//             renderModalWithoutFunction(i);
+//         }
+//     }
+// }
