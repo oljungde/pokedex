b37311986f4e409add77afbeeaef56c1406bea8f
oljungde <oljung@posteo.de>
2022-10-26 21:47:04 +0200

    skill chart finished

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ let currentPokemon = 0;
 let searchInput = document.getElementById('search');
 let searchedPokemon = [];
 let isSearchResult = false;
+
+
 let pokemonOverlayBorder = [
     {
         normal: '#474734',
@@ -26,8 +28,61 @@ let pokemonOverlayBorder = [
         steel: '#63636E',
         fairy: '#99518D',
     }
-]
+];
 
+
+Chart.register(ChartDataLabels);
+let statsChartLabels = ['HP', 'Attack', 'Defense', 'Special-Attack', 'Special-Defense', 'Speed']
+let statsChartData = {
+    labels: statsChartLabels,
+    datasets: [{
+        label: '',
+        borderSkipped: '',
+        borderRadius: 8,
+        backgroundColor: [
+            'rgb(40, 167, 69)',
+            'rgb(220, 53, 69)',
+            'rgb(121, 182, 185)',
+            'rgb(220, 53, 69)',
+            'rgb(121, 182, 185)',
+            'rgb(255, 153, 51)',
+        ],
+        borderColor: 'rgb(255, 99, 132)',
+        data: [],
+    }]
+};
+
+
+const statsChartConfig = {
+    type: 'bar',
+    data: statsChartData,
+    options: {
+        indexAxis: 'y',
+        scales: {
+            xAxis: {
+                display: false
+            },
+            yAxis: {
+                grid: {
+                    display: false,
+                    borderWidth: 0
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            },
+            datalabels: {
+                display: true,
+                color: 'rgb(239, 239, 239)',
+            },
+            tooltip: {
+                enabled: false
+            }
+        }
+    }
+};
 
 
 /**
@@ -145,7 +200,7 @@ function renderPokemonData(pokemonIndex) {
  * @param {number} pokemonIndex is the index of the pokemon from array "loadedPokemon"
  * @param {string} pokemonImage is the url to load the image from the pokemon
  * @param {string} pokemonType0 is the type of the pokemon, e.g. grass
- * @returns the html code to render the name an type of a pokemon
+ * @returns the html code to render the name and type of a pokemon
  */
 function pokemonDataTemplate(pokemonIndex, pokemonImage, pokemonType0) {
     return `
@@ -426,7 +481,6 @@ function pokemonDetailsTemplate(pokemonType0) {
                         <h3>Moves</h3>
                     </span>              
                 </div>
-
             </div>
 
         </div>
@@ -487,10 +541,9 @@ function renderPokemonDetailsBody(pokemonIndex) {
 function pokemonDetailsBodyTemplate(pokemonSize, pokemonKg) {
     return /*html*/`
         <div class="weight-container-overlay">
-                <span><b>size:</b><br>${pokemonSize}m</span>
-                <span><b>weight:</b><br>${pokemonKg}kg</span>
-            </div> 
-        </div>
+            <span><b>size:</b><br>${pokemonSize}m</span>
+            <span><b>weight:</b><br>${pokemonKg}kg</span>
+        </div> 
     `;
 }
 
@@ -555,30 +608,49 @@ function pokemonDetailsStatsContainerTemplate() {
         <div id="pokemon_stats" class="pokemon-abilities">
             <h3>Stats:</h3>
         </div>
+        <div id="chain">
+                
+        </div>
     `;
 }
 
 
-function renderPokemonDetailsStats(pokemonIndex) {
-    renderPokemonDetailsStatsContainer();
-    let pokemonDetailsStatsContainer = document.getElementById('pokemon_stats');
-    let pokemonStats = loadedPokemon[pokemonIndex]['stats'];
-    for (let i = 0; i < pokemonStats.length; i++) {
-        const pokemonStat = pokemonStats[i];
-        pokemonDetailsStatsContainer.innerHTML += pokemonDetailsStatTemplate(pokemonStat);
+function getPokemonStats(pokemonIndex) {
+    let pokemon = loadedPokemon[pokemonIndex];
+    statsChartData['datasets'][0]['data'] = [];
+    for (let i = 0; i < pokemon['stats'].length; i++) {
+        const pokemonStat = pokemon['stats'][i]['base_stat'];
+        statsChartData['datasets'][0]['data'].push(pokemonStat);
     }
 }
 
 
-function pokemonDetailsStatTemplate(pokemonStat) {
+function renderPokemonDetailsStats(pokemonIndex) {
+    getPokemonStats(pokemonIndex);
+    renderPokemonDetailsStatsContainer();
+    let pokemonDetailsStatsContainer = document.getElementById('pokemon_stats');
+    pokemonDetailsStatsContainer.innerHTML += pokemonDetailsStatTemplate();
+    const statsChart = new Chart(
+        document.getElementById('stats_chart'),
+        statsChartConfig
+    );
+}
+
+
+
+
+
+function pokemonDetailsStatTemplate() {
     return /*html*/`
-        <span>${pokemonStat['stat']['name']}: ${pokemonStat['base_stat']}</span ><br>
+        <div>
+            <canvas id="stats_chart"></canvas>
+        </div>
     `;
 }
 
 
 function renderPokemonDetailsEvolutionChainContainer() {
-    let pokemonDetailsBottomContainer = document.getElementById('pokemon_details_bottom');
+    let pokemonDetailsBottomContainer = document.getElementById('chain');
     pokemonDetailsBottomContainer.innerHTML += pokemonDetailsEvolutionChainContainerTemplate();
 }
 
